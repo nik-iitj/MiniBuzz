a3e4f45144e0e300bfa2f806e3532ad93f6175f0
nik-iitj <raj.14@iitj.ac.in>
2021-03-19 21:02:46 +0530

    Login basic design done

--- app/src/main/java/com/example/minibuzz/SignInActivity.java
+++ app/src/main/java/com/example/minibuzz/SignInActivity.java
@@ -2,13 +2,20 @@ package com.example.minibuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
-
+    TextView txtTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        txtTitle=(TextView)findViewById(R.id.txtTitle);
+        //txtTitle.setShadowLayer(50, 0, 0, Color.WHITE);
+
+
     }
 }
