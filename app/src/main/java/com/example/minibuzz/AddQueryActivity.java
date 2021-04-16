90b5c2a7b02ec620726e769f52b1aebe4bc1796f
nik-iitj <raj.14@iitj.ac.in>
2021-04-16 19:24:54 +0530

    Upload Post done...thumbnails remain

--- app/src/main/java/com/example/minibuzz/AddQueryActivity.java
+++ app/src/main/java/com/example/minibuzz/AddQueryActivity.java
@@ -13,33 +13,48 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.OpenableColumns;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AddQueryActivity extends AppCompatActivity {
     private static final int RESULT_LOAD_IMAGE = 1 ;
     TextView title;
-    ImageView attach,show;
+    EditText query;
+    ImageView attach,show,thumb;
     Button post;
     RecyclerView upload;
     FirebaseFirestore firebaseFirestore;
     FirebaseAuth firebaseAuth;
+    StorageReference storageReference;
+    Uri img_upload_uri;
+
 
 
     private List<String> fileNameList,fileDoneList;
     private UploadListAdapter uploadListAdapter;
+    private List<String> urls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,9 +68,13 @@ public class AddQueryActivity extends AppCompatActivity {
         attach=(ImageView)findViewById(R.id.attach_img_btn);
         show=(ImageView)findViewById(R.id.show_img_btn);
         post = (Button)findViewById(R.id.post_btn);
+        thumb=(ImageView)findViewById(R.id.uploadImg);
+        storageReference= FirebaseStorage.getInstance().getReference();
+        query=(EditText)findViewById(R.id.edit_des_text);
 
         fileNameList=new ArrayList<>();
         fileDoneList=new ArrayList<>();
+        urls=new ArrayList<>();
 
         uploadListAdapter= new UploadListAdapter(fileNameList,fileDoneList);
 
@@ -98,7 +117,32 @@ public class AddQueryActivity extends AppCompatActivity {
         post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String text=query.getText().toString();
 
+                if(!TextUtils.isEmpty(text)){
+                    Map<String,Object>postMap = new HashMap<>();
+
+                    postMap.put("Query",text);
+                    postMap.put("Images",urls);
+                    postMap.put("User_ID",firebaseAuth.getCurrentUser().getUid());
+                    postMap.put("TimeStamp", FieldValue.serverTimestamp());
+
+                    firebaseFirestore.collection("Posts").add(postMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(AddQueryActivity.this, "Uploading done", Toast.LENGTH_SHORT).show();
+                            }
+
+                            else{
+                                Toast.makeText(AddQueryActivity.this, "error in uploading", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                } else {
+                    Toast.makeText(AddQueryActivity.this, "Please write something !! ", Toast.LENGTH_SHORT).show();
+                }
 
 
 
@@ -121,10 +165,28 @@ public class AddQueryActivity extends AppCompatActivity {
 
                 for(int i=0;i<total;i++){
                     Uri fileUri = data.getClipData().getItemAt(i).getUri();
-
+                    //thumb.setImageURI(fileUri);
                     String filename= getFilename(fileUri);
                     fileNameList.add(filename);
                     uploadListAdapter.notifyDataSetChanged();
+
+                    StorageReference fileToUpload = storageReference.child("Post_Images").child(filename);
+                    fileToUpload.putFile(fileUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(AddQueryActivity.this, "Done (Storage Reference)", Toast.LENGTH_SHORT).show();
+                            fileToUpload.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    img_upload_uri = uri;
+                                    urls.add(uri.toString());
+
+                                }
+                            });
+                        }
+                    });
+
+
 
                 }
 
@@ -135,6 +197,22 @@ public class AddQueryActivity extends AppCompatActivity {
                 String filename = getFilename(fileUri);
                 fileNameList.add(filename);
                 uploadListAdapter.notifyDataSetChanged();
+
+                StorageReference fileToUpload = storageReference.child("Post_Images").child(filename);
+                fileToUpload.putFile(fileUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(AddQueryActivity.this, "Done (Storage Reference)", Toast.LENGTH_SHORT).show();
+                        fileToUpload.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                img_upload_uri = uri;
+                                urls.add(uri.toString());
+
+                            }
+                        });
+                    }
+                });
 
             }
         }
