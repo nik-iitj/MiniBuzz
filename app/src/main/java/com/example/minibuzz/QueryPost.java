ce116a061eb6b201551bfde6684ae84f08eb8cb4
nik-iitj <raj.14@iitj.ac.in>
2021-05-02 04:56:34 +0530

    post retrieving (a step closer)

--- app/src/main/java/com/example/minibuzz/QueryPost.java
+++ app/src/main/java/com/example/minibuzz/QueryPost.java
@@ -3,42 +3,61 @@ package com.example.minibuzz;
 
 public class QueryPost {
 
-    private String User_Id , Query, Date_Time ;
+    private String User_ID , Query, Date_Time ;
 
 
 
-    public QueryPost(){}
+    public QueryPost(){
 
-    public QueryPost (String User_id, String Query, String Date_Time){
+
+    }
+
+
+
+    public QueryPost (String User_ID, String Query, String Date_Time){
 
         this.Date_Time=Date_Time;
-        this.User_Id=User_id;
+        this.User_ID=User_ID;
         this.Query=Query;
 
     }
 
 
+
+
+
+
     public String getDate_Time() {
+
         return Date_Time;
     }
 
     public void setDate_Time(String date_Time) {
+
         Date_Time = date_Time;
     }
 
     public String getQuery() {
+
         return Query;
     }
 
     public void setQuery(String query) {
+
         Query = query;
+    }
+    public void setUser_Id(String user_ID) {
+
+        User_ID = user_ID;
     }
 
     public String getUser_Id() {
-        return User_Id;
+        return User_ID;
     }
 
-    public void setUser_Id(String user_Id) {
-        User_Id = user_Id;
-    }
+
+
 }
+
+
+
--- app/src/main/java/com/example/minibuzz/QueryRecyclerAdapter.java
+++ app/src/main/java/com/example/minibuzz/QueryRecyclerAdapter.java
@@ -1,18 +1,33 @@
 package com.example.minibuzz;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.Console;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
 public class QueryRecyclerAdapter extends RecyclerView.Adapter<QueryRecyclerAdapter.ViewHolder> {
+
+    FirebaseFirestore firebaseFirestore;
+
 
 
     public List<QueryPost> Query_list ;
@@ -27,6 +42,8 @@ public class QueryRecyclerAdapter extends RecyclerView.Adapter<QueryRecyclerAdap
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.query_list_item ,parent, false) ;
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
 
         return new ViewHolder(view);
     }
@@ -35,8 +52,44 @@ public class QueryRecyclerAdapter extends RecyclerView.Adapter<QueryRecyclerAdap
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
         String query_data = Query_list.get(position).getQuery() ;
-
         holder.setQueryText(query_data);
+
+
+        String user_id = Query_list.get(position).getUser_Id();
+        String time = Query_list.get(position).getDate_Time();
+
+
+
+        firebaseFirestore.collection("Users").document(Query_list.get(position).getUser_Id()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+
+                    if(task.getResult().getString("image")==null){
+                        holder.setUserIncompleteData(task.getResult().getString("name"),time);
+                    }
+
+                    else{
+
+                        holder.setUserCompleteData(task.getResult().getString("name"),task.getResult().getString("image"),time);
+                    }
+
+
+                } else{
+
+                }
+            }
+        });
+
+
+        holder.qView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String k =Query_list.get(position).getUser_Id();
+
+                Toast.makeText(holder.qView.getContext(), k, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -50,6 +103,8 @@ public class QueryRecyclerAdapter extends RecyclerView.Adapter<QueryRecyclerAdap
 
         //private TextView descView;
         private View qView ;
+        TextView username,timestamp;
+        CircleImageView dp;
 
         private TextView queryView ;
 
@@ -65,6 +120,37 @@ public class QueryRecyclerAdapter extends RecyclerView.Adapter<QueryRecyclerAdap
             queryView.setText(queryText);
 
         }
+
+        public void setUserCompleteData(String name, String image, String time){
+
+            username = (TextView)qView.findViewById(R.id.userName);
+            dp = (CircleImageView)qView.findViewById(R.id.dp);
+            timestamp = (TextView)qView.findViewById(R.id.timStamp);
+
+            username.setText(name);
+            timestamp.setText(time);
+
+            Glide.with(qView.getContext()).load(image).into(dp);
+
+
+        }
+
+        public void setUserIncompleteData(String name, String time){
+
+            username = (TextView)qView.findViewById(R.id.userName);
+            timestamp = (TextView)qView.findViewById(R.id.timStamp);
+
+            timestamp.setText(time);
+            username.setText(name);
+
+
+
+
+        }
+
+
+
     }
+
 
 }
